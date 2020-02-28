d813fecd6d6ef97c67c419fbbb957bcd5a31f3d1
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-02-28 10:12:13 -0700

    authconfig

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "agilfoy.auth0.com";
+export const audience = "https://new-brew-server.com";
+export const clientId = "LRHOOJvyj348LNp7u1kDjVBOA5RtzsB1";
