230e4d9d29b598bf55c8845edd36c54823f325af
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-02-28 14:35:01 -0700

    buttonbar looks good, needs styling

--- clientsrc/src/components/buttonBar.vue
+++ clientsrc/src/components/buttonBar.vue
@@ -1,32 +1,58 @@
 <template>
-  <div class="row button-row mx-1">
-    <!-- TODO  at click show new comment component -->
-    <div class="col-3 p-1">
-      <button type="button" class="btn btn-block btn-secondary">Comment</button>
-    </div>
-    <div class="col-3 p-1">
-      <!-- TODO  v if this is the users post -->
-      <button class="btn btn-block btn-danger" @click="deletePost">Delete</button>
-    </div>
-    <div class="col-3 p-1">
-      <button class="btn btn-block btn-success" @click="likePost">Like</button>
-    </div>
-    <div class="col-3 p-1">
-      <button class="btn btn-block btn-warning" @click="dislikePost">Dislike</button>
+  <div class="container-fluid">
+    <div class="row button-row px-1">
+      <!-- TODO  at click show new comment component -->
+      <div class="col-3 m-0 p-1">
+        <button
+          v-if="!commentForm"
+          @click="commentForm = true"
+          type="button"
+          class="btn btn-block btn-secondary"
+        >Comment</button>
+        <button
+          v-if="commentForm"
+          @click="commentForm = false"
+          type="button"
+          class="btn btn-block btn-danger"
+        >Ditch Comment</button>
+      </div>
+      <div class="col-3 m-0 p-1">
+        <!-- TODO  v if this is the
+        users post-->
+        <button class="btn btn-block btn-danger">Delete</button>
+      </div>
+      <div class="col-3 m-0 p-1">
+        <button class="btn btn-block btn-success">Like</button>
+      </div>
+      <div class="col-3 m-0 p-1">
+        <button class="btn btn-block btn-warning">Dislike</button>
+      </div>
     </div>
     <div class="row">
       <!-- TODO add show if new comment is true need to add new comment to the data() -->
-      <new-comment />
+      <create-comment v-if="commentForm" />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import createComment from "@/components/createComment";
+export default {
+  name: "buttonBar",
+  data() {
+    return {
+      commentForm: false
+    };
+  },
+  components: {
+    createComment
+  }
+};
 </script>
 
 <style scoped>
 .button-row {
   justify-content: center;
+  max-width: 100%;
 }
 </style>
--- clientsrc/src/components/newComment.vue
+++
@@ -1,9 +0,0 @@
-            <form @submit="">
-              <textarea class="form-control" id="comment-text" rows="3"></textarea>
-              <button
-                type="button"
-                class="btn btn-secondary float-right"
-                data-dismiss="modal"
-              >Cancel</button>
-              <button type="submit" class="btn btn-primary float-right">Save</button>
-            </form>
